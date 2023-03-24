d8eaa6eef8e630d4a1cc45b579dc439f94db9272
Victor Silva <victorguidasilva@gmail.com>
2023-03-24 12:57:52 -0300

    celery_app.py commented

--- celery_app.py
+++ celery_app.py
@@ -1,4 +1,4 @@
-from celery import Celery
+""" from celery import Celery
 
 app = Celery('tasks', broker='amqp://guest:guest@rabbitmq:5672//')
 
@@ -8,4 +8,4 @@ def add(x, y):
 
 if __name__ == '__main__':
     result = add.delay(4, 4)
-    print(f'Result: {result.get()}')
+    print(f'Result: {result.get()}') """
